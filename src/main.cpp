c1c0c795cd041937fa7a21dd1da01901c8392642
yawkar <yawkarpov@gmail.com>
2023-03-30 13:54:37 +0300

    feat: add runner

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
-#include <fmt/core.h>
+#include <chrono>
+#include <iostream>
+#include <thread>
 
-int main() {
-  fmt::print("The answer is: {}\n", 42);
+#include "chip8/Chip8.hpp"
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    return 0;
+  }
+  chips::Chip8 chip;
+  chip.load_ROM(argv[1]);
+  while (true) {
+    chip.cycle();
+    std::string out;
+    for (int i = 0; i < chip.VIDEO_HEIGHT; ++i) {
+      for (int j = 0; j < chip.VIDEO_WIDTH; ++j) {
+        out += (chip.video[i * chip.VIDEO_WIDTH + j] ? "⬜" : "⬛");
+      }
+      out += '\n';
+    }
+    std::cout << out;
+
+    for (int i = 0; i < 16; ++i) {
+      chip.keypad[i] = false;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(16));
+    std::cout << "\x1B[2J\x1B[H";
+  }
   return 0;
 }
